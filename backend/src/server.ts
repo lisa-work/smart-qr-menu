72f418e400482939398397c6068972063be61080
lisa-work <ngocanhthu.work@gmail.com>
2026-07-06 15:38:34 -0400

    initialize server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});

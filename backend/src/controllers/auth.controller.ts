b4193791e55d789b5471ac2fd0bb265433b12784
lisa-work <ngocanhthu.work@gmail.com>
2026-07-07 15:58:34 -0400

    initialize zod input validation

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
+import { registerSchema } from "../validators/auth.validation";
 import { registerUser } from "../services/auth.service";
 
 export const register = async (req: Request, res: Response) => {
-    const result = await registerUser();
+    const validatedData = registerSchema.safeParse(req.body);
 
-    res.status(200).json(result)
+    // const result = await registerUser();
+
+    res.status(200).json({
+        message: "Validation passed",
+        data: validatedData
+    })
 }
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,5 +1,21 @@
-export const registerUser = async () => {
-    return (
-        {message: "Service reached successfully!"}
-    )
+import { prisma } from "../config/prisma";
+
+type RegisterData = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const registerUser = async (userData: RegisterData) => {
+    const { name, email, password } = userData;
+
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    })
+
+    if (existingUser) {
+        throw new Error("User already exists")
+    }
 }

b8cbe8d9faf0ac2874a28e5e873211e28b02b8a6
lisa-work <ngocanhthu.work@gmail.com>
2026-07-08 16:17:26 -0400

    implement logout

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { register, login } from "../controllers/auth.controller";
 import { protect } from "../middlewares/auth.middleware";
-import { getCurrentUser } from "../services/auth.service";
+import { getCurrentUser, logoutUser } from "../services/auth.service";
 
 const router = Router();
 
+// Authentication routes
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getCurrentUser);
+router.post("/logout", logoutUser)
 
 export default router;
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -84,9 +84,24 @@ export const loginUser = async (loginData: LoginData) => {
     return safeUser;
 }
 
+// Service function to get the current logged-in user
 export const getCurrentUser = (req: Request, res: Response) => {
     return res.json({
         success: true,
         user: req.user,
     });
 };
+
+// Service function to handle user logout
+export const logoutUser = (req: Request, res: Response) => {
+    res.clearCookie("token", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+    });
+
+    return res.status(200).json({
+        success: true,
+        message: "User logged out successfully",
+    });
+}

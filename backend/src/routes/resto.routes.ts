71ce9a3594d7fe184d20a491187f1abc3ca38cd8
lisa-work <ngocanhthu.work@gmail.com>
2026-07-15 11:58:57 -0400

    fix: restaurant protected routes

--- backend/src/routes/resto.routes.ts
+++ backend/src/routes/resto.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createRestaurant, getRestaurant, updateRestaurantInfo } from "../controllers/resto.controller";
+import { protect } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 // Resto routes
-router.post("/create", createRestaurant);
-router.get("/get", getRestaurant);
-router.put("/update", updateRestaurantInfo);
+router.post("/create", protect, createRestaurant);
+router.get("/get", protect, getRestaurant);
+router.put("/update", protect,updateRestaurantInfo);
 
 export default router;

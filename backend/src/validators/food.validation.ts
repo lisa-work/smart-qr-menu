665f97a456012c3ab9d9d4ceb0bcdc4672822fdf
lisa-work <ngocanhthu.work@gmail.com>
2026-07-16 19:33:55 -0400

    add categoryid to foodValidation

--- backend/src/validators/food.validation.ts
+++ backend/src/validators/food.validation.ts
@@ -5,8 +5,11 @@ export const foodValidation = z.object({
     description: z.string().trim().max(200, "Description must be less than 200 characters").optional(),
     price: z.number().positive("Price must be a positive number"),
     image: z.url("Invalid image URL").optional(),
-    available: z.boolean(),
-    featured: z.boolean(),
+    available: z.boolean().optional(),
+    featured: z.boolean().optional(),
+    categoryId: z.number()
+    .int("Invalid category")
+    .positive("Invalid category"),
 });
 
 export const updateFoodValidation = foodValidation.partial();

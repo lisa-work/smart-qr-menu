fb9b629b78af5e0c872801a4f40b8dd3d9e9c3eb
lisa-work <ngocanhthu.work@gmail.com>
2026-07-16 19:02:09 -0400

    add food validation

---
+++ backend/src/validators/food.validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const foodValidation = z.object({
+    name: z.string().trim().min(1, "Food name is required").max(50, "Food name must be less than 50 characters"),
+    description: z.string().trim().max(200, "Description must be less than 200 characters").optional(),
+    price: z.number().positive("Price must be a positive number"),
+    image: z.url("Invalid image URL").optional(),
+    available: z.boolean(),
+    featured: z.boolean(),
+});
+
+export const updateFoodValidation = foodValidation.partial();

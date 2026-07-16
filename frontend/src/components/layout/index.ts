51d9d51eaadf49b751ff492397e878de6ad0dcbf
lisa-work <ngocanhthu.work@gmail.com>
2026-07-16 17:08:57 -0400

    add Category index

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthLayout } from './AuthLayout';
 export { default as MainLayout } from './MainLayout';
 export { default as SettingsLayout } from './SettingsLayout';
+export { default as CategoryLayout } from './CategoryLayout';

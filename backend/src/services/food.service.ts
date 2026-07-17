5b4d52382ee5775d1c30bc83324db3f1af36fcc1
lisa-work <ngocanhthu.work@gmail.com>
2026-07-16 20:07:18 -0400

    implement food service

---
+++ backend/src/services/food.service.ts
@@ -0,0 +1,87 @@
+import { prisma } from "../config/prisma";
+import { getRestaurantOrThrow, getCategoryOrThrow, getFoodOrThrow } from "../utils";
+import { foodValidation, updateFoodValidation } from "../validators/food.validation";
+import { z } from "zod";
+
+export type CreateFoodData = z.infer<typeof foodValidation>;
+export type UpdateFoodData = z.infer<typeof updateFoodValidation>;
+
+export const createFood = async (ownerId: number, foodData: CreateFoodData) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    await getCategoryOrThrow(foodData.categoryId, restaurant.id);
+        const newFood = await prisma.food.create({
+            data: {
+                ...foodData
+            },
+        });
+        return newFood;
+}
+
+export const getFoods = async (ownerId: number) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    const food = await prisma.food.findMany({
+        where: {
+            category: {
+                restaurantId: restaurant.id
+            }
+        },
+        include: {
+            category: true
+        },
+        orderBy: {
+            name: "asc"
+        }
+    })
+    return food;
+}
+
+export const getFoodById =  async (ownerId: number, foodId: number) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    const food = await getFoodOrThrow(foodId, restaurant.id);
+    return food;
+}
+
+export const getFoodByCategoryId = async (ownerId: number, categoryId: number) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    const category = await getCategoryOrThrow(categoryId, restaurant.id);
+    const food = prisma.food.findMany({
+        where: {
+            categoryId: category.id
+        },
+        orderBy: {
+        name: "asc"
+        }
+    })
+    return food;
+}
+
+export const updateFoodById = async (ownerId: number, foodId: number, updateData: UpdateFoodData) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    await getFoodOrThrow(foodId, restaurant.id);
+    if (updateData.categoryId) {
+        await getCategoryOrThrow(
+            updateData.categoryId,
+            restaurant.id
+        );
+    }
+    const food = await prisma.food.update({
+        where: {
+            id: foodId,
+        },
+        data: {
+            ...updateData
+        }
+    })
+    return food;
+}
+
+export const deleteFoodById = async (ownerId: number, foodId: number) => {
+    const restaurant = await getRestaurantOrThrow(ownerId);
+    await getFoodOrThrow(foodId, restaurant.id);
+    const food = await prisma.food.delete({
+        where: {
+            id: foodId,
+        }
+    })
+    return food;
+}

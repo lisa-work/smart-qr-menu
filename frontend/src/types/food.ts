f61787c1e78b81c2acdc705361f26527abc9afd2
lisa-work <ngocanhthu.work@gmail.com>
2026-07-17 20:30:41 -0400

    update food type

--- frontend/src/types/food.ts
+++ frontend/src/types/food.ts
@@ -6,19 +6,18 @@ export type UpdateFoodData = z.infer<typeof updateFoodSchema>;
 
 type FoodFields = {
     label: string;
-    id: string;
+    id: keyof FoodData;
     type: "text" | "textarea" | "number" | "url" | "checkbox" | "file" | "select"| "toggle";
     placeholder?: string;
     required?: boolean;
 }
 
 export const FoodFields: FoodFields[] = [
-    { label: "Image", id: "image", type: "file" },
+    { label: "Image", id: "image", type: "url" },
     { label: "Name", id: "name", type: "text", placeholder: "Enter food name", required: true },
-    { label: "Description", id: "description", type: "textarea", placeholder: "Enter food description", required: true },
-    { label: "Price", id: "price", type: "number", placeholder: "Enter food price", required: true },
-    { label: "Category", id: "category", type: "select", required: true },
+    { label: "Description", id: "description", type: "textarea", placeholder: "Enter food description"},
+    { label: "Price", id: "price", type: "number", placeholder: "0.00", required: true },
+    { label: "Category", id: "categoryId", type: "select", required: true },
     { label: "Available", id: "available", type: "toggle"},
-    { label: "Daily Special", id: "featured", type: "toggle"},
-
+    { label: "Featured Item", id: "featured", type: "toggle"},
 ]

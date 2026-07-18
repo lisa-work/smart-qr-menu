a21e40c3575f70bfe56a046704da9de2c039f06e
lisa-work <ngocanhthu.work@gmail.com>
2026-07-17 20:30:30 -0400

    update food schema

--- frontend/src/schema/food.ts
+++ frontend/src/schema/food.ts
@@ -11,7 +11,7 @@ export const foodSchema = z.object({
         .max(200, "Description must be less than 200 characters")
         .optional(),
 
-    price: z.coerce.number()
+    price: z.number()
         .positive("Price must be a positive number"),
 
     image: z.string()

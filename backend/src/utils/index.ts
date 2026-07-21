20d0289262eb0210579157ca8ebbb2ea155b3bd6
lisa-work <ngocanhthu.work@gmail.com>
2026-07-21 17:18:09 -0400

    add slug to index

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -3,3 +3,5 @@ export { getCategoryOrThrow } from "./category.helper";
 export { getRestaurantOrThrow } from "./restaurant.helper";
 export { getUserId } from "./getUserId.helper";
 export { getFoodOrThrow } from "./food.helper";
+export { generateSlug } from "./slug";
+export { ensureSlugAvailable } from "./slug";

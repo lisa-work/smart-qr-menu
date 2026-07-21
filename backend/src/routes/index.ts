e0912f50b88ac492ef7c056134d27a979a836462
lisa-work <ngocanhthu.work@gmail.com>
2026-07-21 17:50:46 -0400

    add menuRoutes to index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,3 +2,4 @@ export { default as authRoutes } from "./auth.routes";
 export { default as restoRoutes } from "./resto.routes";
 export { default as categoryRoutes } from "./category.routes";
 export { default as foodRoutes } from "./food.routes";
+export { default as menuRoutes } from "./menu.routes";

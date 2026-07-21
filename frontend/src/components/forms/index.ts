4d8adb652f4a078e00ad329585455905e8184d71
lisa-work <ngocanhthu.work@gmail.com>
2026-07-21 16:52:17 -0400

    add food form index

--- frontend/src/components/forms/index.ts
+++ frontend/src/components/forms/index.ts
@@ -2,3 +2,4 @@ export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 export { default as SettingsForm } from './SettingsForm';
 export { default as CategoryForm } from './CategoryForm';
+export { default as FoodsForm } from './FoodsForm';

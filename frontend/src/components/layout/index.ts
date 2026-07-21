b98ab8282fbe5844cb7e8ae9a366159c30a985e7
lisa-work <ngocanhthu.work@gmail.com>
2026-07-21 16:51:53 -0400

    add food index

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -2,3 +2,5 @@ export { default as AuthLayout } from './AuthLayout';
 export { default as MainLayout } from './MainLayout';
 export { default as SettingsLayout } from './SettingsLayout';
 export { default as CategoryLayout } from './CategoryLayout';
+export { default as FoodsLayout } from './FoodsLayout';
+export { default as FoodList } from './FoodList';

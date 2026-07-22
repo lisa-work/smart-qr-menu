c5c2784f5ef319ef5d2de7ac637676be97d25f92
lisa-work <ngocanhthu.work@gmail.com>
2026-07-22 11:46:05 -0400

    add upload middleware to food routes

--- backend/src/routes/food.routes.ts
+++ backend/src/routes/food.routes.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import { createNewFood, getAllFoods, getFoodListById, getFoodListByCategoryId, updateFoodListById, deleteFoodListById } from "../controllers/food.controller";
 import { protect } from "../middlewares/auth.middleware";
+import upload from "../middlewares/upload.middleware";
 
 const router = Router();
 
-router.post("/", protect, createNewFood);
+router.post("/", protect, upload.single("image"), createNewFood);
 router.get("/", protect, getAllFoods);
 router.get("/:foodId", protect, getFoodListById);
 router.get("/category/:categoryId", protect, getFoodListByCategoryId);

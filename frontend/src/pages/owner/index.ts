9df741de79f042bca778f1f4fd5dc2d4768bce93
lisa-work <ngocanhthu.work@gmail.com>
2026-07-23 18:47:51 -0400

    add pages index

--- frontend/src/pages/owner/index.ts
+++ frontend/src/pages/owner/index.ts
@@ -1,2 +1,5 @@
 export { default as RestaurantSettingsPage } from "./RestaurantSettings";
 export { default as CategoryPage } from "./CategoryPage";
+export { default as FoodPage } from "./FoodPage";
+export { default as DashboardPage } from "./DashboardPage";
+export { default as QRPage } from "./QRPage";

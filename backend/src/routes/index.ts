a3386ea3cf5e26007e4ddc009ca80574bafe7de3
lisa-work <ngocanhthu.work@gmail.com>
2026-07-23 18:45:17 -0400

    add QR routes to routes index

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,3 +4,4 @@ export { default as categoryRoutes } from "./category.routes";
 export { default as foodRoutes } from "./food.routes";
 export { default as menuRoutes } from "./menu.routes";
 export { default as dashboardRoutes } from "./dashboard.routes";
+export { default as qrRoutes } from "./qr.routes";

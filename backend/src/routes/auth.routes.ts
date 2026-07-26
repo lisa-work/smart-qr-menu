96d056827886dcb90284f9dc8cfc6649f3ddd24e
lisa-work <ngocanhthu.work@gmail.com>
2026-07-25 20:24:16 -0400

    rename getCurrentUser controller

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from "express";
-import { register, login } from "../controllers/auth.controller";
+import { register, login, getCurrent, logoutUser } from "../controllers/auth.controller";
 import { protect } from "../middlewares/auth.middleware";
-import { getCurrentUser, logoutUser } from "../controllers/auth.controller";
 
 const router = Router();
 
 // Authentication routes
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", protect, getCurrentUser);
+router.get("/me", protect, getCurrent);
 router.post("/logout", logoutUser)
 
 export default router;

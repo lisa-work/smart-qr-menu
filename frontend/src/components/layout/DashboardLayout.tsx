64823549474ada0322046f57650020c747bfba06
lisa-work <ngocanhthu.work@gmail.com>
2026-07-26 19:04:41 -0400

    refine DashboardLayout

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -1,11 +1,20 @@
 interface DashboardLayoutProps {
-  children: React.ReactNode;
+    title?: string;
+    subtitle?: string;
+    footer?: string;
+    children: React.ReactNode;
 }
 
-function DashboardLayout({ children }: DashboardLayoutProps) {
+function DashboardLayout({ title, subtitle, footer, children }: DashboardLayoutProps) {
   return (
-    <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
-        {children}
+    <div>
+        <div className="text-left mx-3 md:mx-5 my-5 md:my-10">
+            <h1>{title}</h1>
+            <p className="text-xs md:text-sm lg:text-base text-gray-700">{subtitle}</p>
+        </div>
+        <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-4">
+            {children}
+        </div>
     </div>
   )
 }

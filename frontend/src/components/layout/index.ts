cf996d8d5457b956131cadf60d78a76ee7ef5de0
lisa-work <ngocanhthu.work@gmail.com>
2026-07-27 16:47:11 -0400

    remove abundant Layouts in layout index

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,6 +1,4 @@
 export { default as AuthLayout } from './AuthLayout';
-export { default as SettingsLayout } from './SettingsLayout';
-export { default as CategoryLayout } from './CategoryLayout';
 export { default as FoodsLayout } from './FoodsLayout';
 export { default as FoodList } from './FoodList';
-export { default as DashboardLayout } from './DashboardLayout';
+export { default as GeneralLayout } from './GeneralLayout';

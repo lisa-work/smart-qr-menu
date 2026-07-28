22dd432882ce3398aba25557c815fc4a661e32b4
lisa-work <ngocanhthu.work@gmail.com>
2026-07-28 11:30:12 -0400

    add QRLayout

---
+++ frontend/src/components/layout/QRLayout.tsx
@@ -0,0 +1,22 @@
+import type { LayoutProps } from '@/types/layout'
+
+function QRLayout({ title, subtitle, footer, children }: LayoutProps) {
+    return (
+        <div className="m-5">
+            <div className="text-left mx-3 md:mx-5 my-5 md:my-10">
+                <h1>{title}</h1>
+                <p className="text-xs md:text-sm lg:text-base text-gray-700">{subtitle}</p>
+            </div>
+            <div>
+                {children}
+            </div>
+            {footer && (
+                <div className="text-xs text-muted-foreground">
+                    {footer}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default QRLayout

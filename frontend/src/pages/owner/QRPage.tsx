2c22c96a6ece4a2f4f5b19f3522bcfc16039631f
lisa-work <ngocanhthu.work@gmail.com>
2026-07-28 11:30:33 -0400

    refine QRPage

--- frontend/src/pages/owner/QRPage.tsx
+++ frontend/src/pages/owner/QRPage.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import qrService from "@/services/qr";
 import { useParams } from "react-router-dom";
+import { QRLayout } from "@/components/layout";
+import { Button } from "@/components";
 
 function QRPage() {
     const [loading, setLoading] = useState(false);
@@ -36,15 +38,28 @@ function QRPage() {
         <div>
                 {loading && <p>Loading...</p>}
                 {!loading && qrCode && (
-                    <div>
-                        <img src={qrCode} alt="QR Code" />
-                        <a href={qrCode} download={`qr-code-${slug}.png`}>
-                            Download QR Code
-                        </a>
-                </div>
+                    <QRLayout title="QR Code" subtitle="Print this QR code so customers can scan and view your menu">
+                        <div className="flex flex-col items-center justify-center space-y-4 mx-auto px-auto">
+                            <img src={qrCode} alt="QR Code" 
+                            className="mx-auto border rounded-lg shadow-md hover:shadow-lg p-3 size-60 md:size-80" />
+                            <div className="flex flex-row gap-2">
+                                <Button className="bg-white text-black hover:text-white hover:font-bold rounded-sm">
+                                    <a href={qrCode} download={`qr-code-${slug}.png`}>
+                                        Download QR Code
+                                    </a>
+                                </Button>
+                                <Button className="rounded-sm cursor-pointer">
+                                    <a>
+                                        Preview Menu
+                                    </a>
+                                </Button>
+                            </div>
+                        </div>
+                    </QRLayout>
                 )}
 
         </div>
+        
     )
 }
 

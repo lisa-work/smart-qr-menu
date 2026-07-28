6847ed377814cb3d74f4d9a5f3dcc182093744ec
lisa-work <ngocanhthu.work@gmail.com>
2026-07-28 11:25:40 -0400

    add components index

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Logo } from './Logo';
+export { default as Modal } from './Modal';
+export { default as NavBar } from './NavBar';

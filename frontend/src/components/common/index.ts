8319c2d2c506edd53560af4860021a580f09aa8b
lisa-work <ngocanhthu.work@gmail.com>
2026-07-31 18:51:21 -0400

    add SideBarNav to components common index

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Dashboard } from './Dashboard';
 export { default as Logo } from './Logo';
 export { default as Modal } from './Modal';
 export { default as NavBar } from './NavBar';
+export { default as SideBarNav } from './SideBar';

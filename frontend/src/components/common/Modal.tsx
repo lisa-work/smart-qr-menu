093de8cd9917157f7a35e9fa4df7deaf2be55034
lisa-work <ngocanhthu.work@gmail.com>
2026-07-31 17:28:21 -0400

    refine Modal

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -14,7 +14,7 @@ function Modal({ children, isOpen, onClose, title }: ModalProps) {
   return (
     <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full 
     h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50">
-      <div className="relative p-4 w-full md:w-[50%] lg:w-[30%] max-w-2xl max-h-full">
+      <div className="relative p-4 w-full md:w-[50%] lg:w-[35%] max-w-2xl max-h-full">
         {/* Modal content */}
         <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
           {/* Modal header */}

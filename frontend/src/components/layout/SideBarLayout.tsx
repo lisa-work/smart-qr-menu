6683be8196bd2cdab9e1ff656d1ceab35748f6e9
lisa-work <ngocanhthu.work@gmail.com>
2026-07-31 19:03:58 -0400

    add handleClick on SideBar button

--- frontend/src/components/layout/SideBarLayout.tsx
+++ frontend/src/components/layout/SideBarLayout.tsx
@@ -1,13 +1,18 @@
 import type {SideBarProps} from "@/types/sidebar";
+import {Button} from "@/components/ui"
+import { useNavigate } from "react-router-dom";
 
 function SideBarLayout({icon, label, path}: SideBarProps) {
   const Icon = icon;
+  const navigate = useNavigate();
 
   return (
-    <div className="flex flex-row gap-4 hover:bg-gray-100 items-center py-2 w-[15%] cursor-pointer text-xs md:text-sm rounded-sm">
+    <Button
+    onClick={() => navigate(path)}
+    className="bg-white text-black flex flex-row items-center justify-start gap-4 hover:bg-gray-100 items-center py-2 w-[15%] cursor-pointer text-xs md:text-sm rounded-sm">
         <Icon size={20}/>
         {label}
-    </div>
+    </Button>
   )
 }
 

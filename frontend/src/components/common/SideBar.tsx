b8c4ae41972bf1f621b793dcbd3ad1bf0fff3efd
lisa-work <ngocanhthu.work@gmail.com>
2026-07-31 19:39:43 -0400

    refine SideBarNav

--- frontend/src/components/common/SideBar.tsx
+++ frontend/src/components/common/SideBar.tsx
@@ -3,15 +3,16 @@ import { SideBarLayout } from "../layout";
 import { MdRestaurantMenu } from "react-icons/md";
 
 function SideBarNav() {
+
   return (
-    <div className="m-5">
+    <div className="flex h-full flex-col gap-6 p-5">
         <div className="flex flex-row gap-4 items-center my-4 border-b pt-2 pb-3">
             <MdRestaurantMenu size={40} color="white" className="bg-black rounded-sm p-1"/>
             <h2 className="text-left text-xs md:text-sm font-bold">MenuHub</h2>
         </div>
-        <div className="">
-            {SideBar.map((item, index) => (
-                <SideBarLayout icon={item.icon} label={item.label} path={item.path} />
+      <div className="flex flex-col gap-2">
+        {SideBar.map((item) => (
+          <SideBarLayout key={item.label} icon={item.icon} label={item.label} path={item.path} />
             ))}
         </div>
     </div>

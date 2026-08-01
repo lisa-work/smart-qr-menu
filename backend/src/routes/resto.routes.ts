60b935dc03e4d6689416336d70aa7dcbf57a254c
lisa-work <ngocanhthu.work@gmail.com>
2026-08-01 11:43:19 -0400

    add image upload to create and update restaurant

--- backend/src/routes/resto.routes.ts
+++ backend/src/routes/resto.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createRestaurant, getRestaurant, updateRestaurantInfo } from "../controllers/resto.controller";
 import { protect } from "../middlewares/auth.middleware";
+import upload from "../middlewares/upload.middleware";
 
 const router = Router();
 
 // Resto routes
-router.post("/", protect, createRestaurant);
+router.post("/", protect, upload.single("logo"), createRestaurant);
 router.get("/", protect, getRestaurant);
-router.put("/", protect, updateRestaurantInfo);
+router.put("/", protect, upload.single("logo"), updateRestaurantInfo);
 
 export default router;

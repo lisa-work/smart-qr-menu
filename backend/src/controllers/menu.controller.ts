787e7473c2ff8de74f0e6f896b3193d3a3537f1b
lisa-work <ngocanhthu.work@gmail.com>
2026-08-02 11:41:05 -0400

    modify menu controller res json

--- backend/src/controllers/menu.controller.ts
+++ backend/src/controllers/menu.controller.ts
@@ -4,11 +4,11 @@ import { getRestaurantBySlug } from "../services/menu.service";
 export const getRestaurant = async (req: Request, res: Response) => {
     const slug = Array.isArray(req.params.slug) ? req.params.slug[0] : req.params.slug;
 
-    const restaurant = await getRestaurantBySlug(slug)
+    const menu = await getRestaurantBySlug(slug)
 
     res.status(200).json({
         success: true,
-        data: restaurant
+        ...menu
     })
 }
 

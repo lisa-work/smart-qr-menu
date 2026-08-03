d1631706f91939f1f96c06a890973f5f2c444319
lisa-work <ngocanhthu.work@gmail.com>
2026-08-03 16:38:55 -0400

    remove the background color in dashboard card

--- frontend/src/components/ui/dashboard-card.tsx
+++ frontend/src/components/ui/dashboard-card.tsx
@@ -2,7 +2,7 @@ import type { DashboardCardProps } from "@/types/dashboard"
 
 function DashboardCard({icon, count, label} : DashboardCardProps) {
   return (
-    <div className="flex flex-row items-center justify-start p-4 bg-white rounded-lg border-2 hover:shadow-md m-2">
+    <div className="flex flex-row items-center justify-start p-4 rounded-lg border-2 hover:shadow-md m-2">
         {icon}
       <div className="flex flex-col justify-start items-start mx-3">
             <h2 className="font-bold">{count}</h2>
